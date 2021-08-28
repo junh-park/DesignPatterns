2c3010717d6af2e6e5a1526a8a99d957bb3393e5
junh-park <jhpar25@hotmail.com>
2021-08-28 23:59:02 +1000

    Updating the commands with lambdas

--- src/main/java/com/jun/behavioural/command/RemoteLoader.java
+++ src/main/java/com/jun/behavioural/command/RemoteLoader.java
@@ -3,25 +3,21 @@ package com.jun.behavioural.command;
 public class RemoteLoader {
 	public static void main(String[] args) {
 		RemoteControl remote = new RemoteControl();
-		
+
 		Light livingRoomLight = new Light("Living Room");
 		Light kitchenLight = new Light("Kitchen");
 		Stereo stereo = new Stereo("Living Room");
-		
-		LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
-		LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
-		LightOnCommand kitchenLightOn = new LightOnCommand(kitchenLight);
-		LightOffCommand kitchenLightOff = new LightOffCommand(kitchenLight);
-		
-		StereoOnWithCDCommand stereoOnWithCD = new StereoOnWithCDCommand(stereo);
-		StereoOffWithCDCommand stereoOff = new StereoOffWithCDCommand(stereo);
-		
-		remote.setCommand(0, livingRoomLightOn, livingRoomLightOff);
-		remote.setCommand(1, kitchenLightOn, kitchenLightOff);
-		remote.setCommand(2, stereoOnWithCD, stereoOff);
-		
+
+		remote.setCommand(0, () -> livingRoomLight.on(), () -> livingRoomLight.off());
+		remote.setCommand(1, () -> kitchenLight.on(), () -> kitchenLight.off());
+		remote.setCommand(2, () -> {
+			stereo.on();
+			stereo.setCD();
+			stereo.setVolume(1);
+		}, () -> stereo.off());
+
 		System.out.println(remote);
-		
+
 		remote.onButtonWasPushed(0);
 		remote.offButtonWasPushed(0);
 		remote.onButtonWasPushed(1);
